73e9332ea197bee1bd8e64c322d72285496c408a
richelbilderbeek <richel@richelbilderbeek.nl>
2023-06-22 10:39:01 +0200

    Use CMake

---
+++ scripts/build_rackham.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 10:00
+#SBATCH --job-name gha_gcc_cpp17
+#SBATCH --output build_rackham.log
+#SBATCH --qos=short
+#
+# Build the project on Rackham, an UPPMAX computer cluster, see
+# https://www.uppmax.uu.se/support/user-guides/rackham-user-guide/
+#
+# Usage:
+#
+#   ./scripts/build_rackham.sh
+#
+#  Using sbatch, when being Richel:
+#
+#   sbatch -A uppmax2023-2-25 -M snowy scripts/build_rackham.sh
+#
+if [[ ! -z "${CLUSTER}" ]]; then
+  echo "Working on a cluster"
+else
+  echo "Not working on a cluster, stopping"
+  exit 42
+fi
+
+module load gcc/13.1.0 cmake/3.26.3
+
+# Build
+./scripts/build.sh
+
+# Run
+./build/gha_cmake_gcc_cpp17_booost
+
